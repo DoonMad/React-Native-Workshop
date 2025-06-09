870da0eaddb756cab2089eca5c483d23d1e43aba
Apoorva Mundada <apoorva.aam@gmail.com>
2025-06-09 12:07:37 +0530

    updated design

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,13 +4,21 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen
-      name="index"
-      options={{
-        title: "BMI Calculator", 
-        headerStyle: {backgroundColor: "#f4511e"}, 
-        headerTintColor: "#fff", 
-        headerTitleAlign: 'center',
-      }}
+        name="index"
+        options={{
+          title: "BMI Calculator",
+          headerStyle: { 
+            backgroundColor: '#0d6bb2',
+          },
+          headerShown: false,
+          headerTintColor: "#fff",
+          headerTitleAlign: 'center',
+          // headerShadowVisible: false,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 20,
+          },
+        }}
       />
     </Stack>
   );
